49aa8f2286b448ed8061e6b97dd9e40564b19646
Joshua Melvin <joshuamlvn@gmail.com>
2023-04-28 11:04:46 +0700

    Create basic navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,22 @@
 import { ReactNode } from "react";
-import Navbar from "./navbar";
+import { VT323 } from "next/font/google";
+import Navbar from "./Navbar";
+import Head from "next/head";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
+const font = VT323({weight: "400", subsets: ['latin']});
+
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Pokemon | Nextdex</title>
+      </Head>
       <Navbar />
-      <main>{children}</main>
+      <main className={`${font.className} bg-gray-50 mt-[114px]`}>{children}</main>
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import logo from "@dex/public/images/Pokemon.svg";
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+  const [clientWindowHeight, setClientWindowHeight] = useState(0);
+
+  const [padding, setPadding] = useState(30);
+  const [boxShadow, setBoxShadow] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    setClientWindowHeight(window.scrollY);
+  };
+
+  useEffect(() => {
+    let backgroundTransparacyVar = clientWindowHeight / 600;
+
+    if (backgroundTransparacyVar < 0.52) {
+      let paddingVar = 2 - backgroundTransparacyVar * 3;
+      let boxShadowVar = backgroundTransparacyVar * 0.1;
+      setPadding(paddingVar);
+      setBoxShadow(boxShadowVar);
+    }
+  }, [clientWindowHeight]);
+
   return (
-    <nav className="pb-5">
-      This is Navbar!
+    <nav className="fixed top-0 left-0 right-0 w-full backdrop-blur-sm z-10">
+      <div
+        className="flex justify-center"
+        style={{
+          padding: `${padding}rem 0rem`,
+          boxShadow: `rgb(0 0 0 / ${boxShadow}) 0px 0px 20px 6px`,
+        }}
+      >
+        <Image src={logo} alt="" height={50} />
+      </div>
     </nav>
-  )
+  );
 }
