c06b8c4026408014cf3f63b2b49472234363390d
Joshua Melvin <joshuamlvn@gmail.com>
2023-05-03 17:39:50 +0700

    WIP card styling

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,8 +23,7 @@ export default function Card({ id, name, types, sprites }: CardProps) {
 
   return (
     <div
-      className="rounded-xl shadow-lg p-6"
-      style={{ backgroundColor: color }}
+      className="rounded-xl shadow-lg p-6 bg-gradient-to-br from-slate-200/40 to-slate-50/30"
     >
       <div className="pb-5 flex justify-center">
         <Image
@@ -35,15 +34,15 @@ export default function Card({ id, name, types, sprites }: CardProps) {
           priority={true}
         />
       </div>
-      <div className="grid grid-cols-2 gap-2">
+      <div className="flex items-center justify-center flex-col gap-2">
         <div>
           <p className="m-0">{formatPokemonId(id)}</p>
-          <h2 className="text-2xl">{name}</h2>
-        </div>
-        <div>
           {types.map((type: PokemonType, index: any) => (
             <Type item={type.type.name} key={index}></Type>
           ))}
+        </div>
+        <div>
+          <h2 className="text-2xl">{name}</h2>
         </div>
       </div>
     </div>
